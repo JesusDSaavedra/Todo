cc22d0117e8e1789543d137c2fc7467917f6ee0b
iam.jesusd88@gmail.com <iam.jesusd88@gmail.com>
2022-05-27 22:05:05 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import './styles.css';
+
+import {Todo,TodoList} from './classes';
+import { crearTodoHtml } from './js/componentes';
+
+
+export const todoList = new TodoList();
+
+todoList.todos[0].imprimirClase();
+
+todoList.todos.forEach(crearTodoHtml);
+
+console.log('todos',todoList.todos)
+
+
